e9fea4865e3974796b3b1b794d61763f10da2e2a
Maikel33 <perezguedesmaikel@gmail.com>
2022-08-03 16:48:02 -0400

    hash navegacion

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import HouseContextProvider from './components/HouseContext';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <HashRouter>
     <HouseContextProvider>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </HouseContextProvider>
-  </Router>
+  </HashRouter>
 );
